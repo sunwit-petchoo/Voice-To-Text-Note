60b750860c34023321a6e1a7b1fd5ae929303867
Sunwit Petchoo <sunwit.p@gmail.com>
2021-07-22 22:34:19 +1000

    Add translation feature

---
+++ public/js/speechRecTranslate.js
@@ -0,0 +1,92 @@
+
+//initialize speech recognition API
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition(); //initialize my instance of speech recognition
+recognition.lang = 'en-US';
+recognition.interimResults = true; //return results while still working on current recognition
+
+//this is where your speech-to-text results will appear
+//let p = document.createElement("p")
+const words = document.querySelector("#wordsContainer")
+//words.appendChild(p)
+
+//I want to select and change the color of the body, but this could be any HTML element on your page
+//let body = document.querySelector("body")
+
+//let cap_css_colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+// const CSS_COLORS = cap_css_colors.map(color => {
+//   //I need to change all color names to lower case, because comparison between words will be case sensitive
+//   return color.toLowerCase()
+// })
+
+//once speech recognition determines it has a "result", grab the texts of that result, join all of them, and add to paragraph
+recognition.addEventListener("result", e => {
+  const transcript = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join("")
+  //p.innerText = transcript
+  
+
+  //once speech recognition determines it has a final result, create a new paragraph and append it to the words-container
+  //this way every time you add a new p to hold your speech-to-text every time you're finished with the previous results
+  if (e.results[0].isFinal) {
+    //p = document.createElement("p")
+    //words.appendChild(p)
+    jQ_append(words,transcript)
+  }
+  //for each result, map through all color names and check if current result (transcript) contains that color
+  //i.e. see if a person said any color name you know
+  // CSS_COLORS.forEach(color => {
+  //   //if find a match, change your background color to that color
+  //   if (transcript.includes(color)) {
+  //     body.style.backgroundColor = color;
+  //   }
+  // }) 
+})
+
+//add your functionality to the start and stop buttons
+function startRecording() {
+  $('#wordsContainer').text("")
+  $('#output_translate').text("")
+  recognition.start();
+  recognition.addEventListener("end", recognition.start)
+
+  document.getElementById("stop").addEventListener("click", stopRecording)
+}
+
+function stopRecording() {
+  console.log("okay I'll stop")
+  recognition.removeEventListener("end", recognition.start)
+  recognition.stop();
+  
+  // make API request 
+  $.ajax({
+    type: "POST",
+    url: "/translation",
+    data: { 
+        input_voice: $('#wordsContainer').val(),
+        selected_lang: $('#selected_lang').val()
+    },
+    success: function(data) {
+    // send alert message when success
+   
+   console.log(data.output_translate)
+   $('#output_translate').text("")
+   $('#output_translate').text(data.output_translate)
+   },
+    error: function(data) {
+        console.log("error")
+    },
+    dataType: "json"
+  });
+
+
+}
+
+//Append text to input element
+function jQ_append(id_of_input, text){
+  var input_id = id_of_input;
+  $(input_id).val($(input_id).val() + " "+text);
+}
--- routes/translation.js
+++ routes/translation.js
@@ -1,14 +1,55 @@
+//Ref: https://www.npmjs.com/package/@vitalets/google-translate-api
+const translate = require('@vitalets/google-translate-api');
+var languages = require('../node_modules/@vitalets/google-translate-api/languages');
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {    
+router.get('/', (req, res) => {  
+    var lang_key = []
+    var lang_value = []
+    
+    for(var key in languages) {
+        value = languages[key];
+        
+        lang_key.push(key)
+        lang_value.push(value)
+    }
+    
     res.render('pages/translation', {
-        layout: './layouts/full-width'
+        layout: './layouts/full-width',
+        lang_key: lang_key,
+        lang_value: lang_value,
+        output_translate: ''
     })
 })
 
 router.post('/', async (req, res) => {
-    
+    const { input_voice, selected_lang } = req.body
+    var output = ""
+    const returnObj = {
+        output_translate: ""
+      }
+
+    await translate(input_voice, {from: 'en', to: selected_lang}).then(res => {
+        output = res.text
+        console.log(output)
+    }).catch(err => {
+        console.error(err);
+    });
+    returnObj['output_translate']  = output
+    return res.send(JSON.stringify(returnObj));
+    // translate('hello', {from: 'auto', to: 'th'}).then(res => {
+    //     console.log(res.text);
+    //     //=> Ik spreek Nederlands!
+    //     console.log(res.from.text.autoCorrected);
+    //     //=> true
+    //     console.log(res.from.text.value);
+    //     //=> I [speak] Dutch!
+    //     console.log(res.from.text.didYouMean);
+    //     //=> false
+    // }).catch(err => {
+    //     console.error(err);
+    // });
 })
 
 module.exports = router
